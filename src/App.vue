24d39f46044a0922f5776ef4be68e9ef87b97936
MichelaRicci <michela.ricci.1991@gmail.com>
2023-01-24 18:06:15 +0100

    header added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
-<script setup>
+<script>
+
+
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+export default {
+
+    components: { AppHeader, AppMain }
+}
 
 </script>
 
 <template>
+    <header>
+        <AppHeader></AppHeader>
+    </header>
+
+    <main>
+        <AppMain></AppMain>
+    </main>
+
 
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,8 +4,49 @@
 
 <template>
 
+    <div class="container">
+
+        <div class="row justify-content-between">
+            <div class="col">
+                <div class="d-flex circle-container">
+                    <div class="circle big-circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                </div>
+            </div>
+
+            <div class="col">
+                <img src="src/assets/img/logo-pokemon.png" alt="logo-pokemon">
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <style lang="scss" scoped>
+img {
+    width: 400px;
+}
 
+.circle-container {
+
+    padding: 10px;
+
+
+    .circle {
+        margin: 0 5px;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        border: 2px solid white;
+    }
+
+    .big-circle {
+
+        width: 100px;
+        height: 100px;
+
+    }
+}
 </style>
